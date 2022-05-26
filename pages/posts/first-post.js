f4055831b578240cdfd54dbcf2f189e48048f484
Suttipong Srikok <suttipongs@toptrader.co.th>
2022-05-26 14:37:58 +0700

    Layout Component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,29 +1,12 @@
-import Script from 'next/script';
 import Head from 'next/head';
 import Link from 'next/link';
-import Image from 'next/image';
-
-const YourComponent = () => (
-    <Image 
-        src="/images/profile.jpg" // Route of the image file
-        height={144} // Desired size with correct aspect ratio
-        width={144} // Desired size with correct aspect ratio
-        alt="Your Name"
-    />
-);
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
-          <title>First Post</title>
-          <Script 
-            src="https://connect.facebook.net/en_US/sdk.js"
-            strategy="lazyOnload"
-            onLoad={() =>
-                console.log(`script loaded correctly, window.FB has been populated`)
-            }
-          />
+        <title>First Post</title>
       </Head>
       <h1>First Post</h1>
       <h2>
@@ -31,8 +14,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-
-      <YourComponent />
-    </>
+    </Layout>
   );
 }
