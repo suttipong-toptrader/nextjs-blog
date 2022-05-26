6b39f6c4f5b263427d81e727b4c11f2b42cb8b9d
Suttipong Srikok <suttipongs@toptrader.co.th>
2022-05-26 12:10:18 +0700

    Using the Image Component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,14 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
+const YourComponent = () => (
+    <Image 
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+    />
+);
 
 export default function FirstPost() {
   return (
@@ -9,6 +19,8 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
+
+      <YourComponent />
     </>
   );
 }
